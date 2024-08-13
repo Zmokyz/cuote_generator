cacba3806041ed9cf826b84156f1313864acb6c8
Zmokyz <ortizzarif2@gmail.com>
2024-08-13 12:32:11 -0600

    added tags from the api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const author = document.getElementById("show_autor")
 const quote = document.getElementById("show_quote")
 const random_button = document.getElementById("button_random")
 const copy_button = document.getElementById("button_copy")
+const tagsdiv = document.getElementById("quote_class")
 
 // Api implemented and his response 
 async function fetchQuote() {
@@ -14,6 +15,18 @@ async function fetchQuote() {
       //we put the data on the right place 
       author.innerText = data.author;
       quote.innerText = data.content;
+      //cleans the container for the tags
+      tagsdiv.innerHTML = "";
+      // variable for the tags
+      const tags = data.tags;
+      //function to append the tags on the app
+      tags.forEach((tag) => {
+        const elemtag = document.createElement('p');
+        elemtag.innerText = tag;
+        elemtag.className = 'button_class'; // style the tag
+        tagsdiv.appendChild(elemtag);
+      })
+
     } //catch the error if there´s one 
      catch (error) {
       console.error('Error:', error);
@@ -27,10 +40,11 @@ async function fetchQuote() {
 random_button.addEventListener("click", function () {
     fetchQuote();
 });
+
 // copy text button 
 copy_button.addEventListener("click", function() {
     copy_text= quote.innerText;
-    //this is actually the thing that make the clipboard copy 8the text 
+    //this is actually the thing that make the clipboard copy the text 
     navigator.clipboard.writeText(copy_text);
     alert("Copied the text: " + copy_text);
 });
